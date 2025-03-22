1b12db3a9808c8c369a6f7aa996ce62bd59f3479
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2025-03-22 14:02:49 +0200

    Add initial files

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'CRUD Operations Dashboard',
+  description:
+    'an intuitive and engaging dashboard with my design, where users can manage products with advanced features like filtering, sorting, pagination, and more.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
